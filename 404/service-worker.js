1d152b9fdec62a52095205aa2f628316b091c217
rstuteam <94618799+rstuteam@users.noreply.github.com>
2022-09-22 12:34:15 +0700

    Update service-worker.js

--- 404/service-worker.js
+++ 404/service-worker.js
@@ -1,7 +1,7 @@
 const OFFLINE_VERSION = 1;
 const CACHE_NAME = "offline";
 // Customize this with a different URL if needed.
-const OFFLINE_URL = "offline.html";
+const OFFLINE_URL = "/404";
 
 self.addEventListener("install", (event) => {
   event.waitUntil(
